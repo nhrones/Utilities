97e9e1d2903c0ce5201a47905df4441a3319950e
NDH <nick@ndhrones.com>
2024-01-20 13:18:14 -0500

    r

--- Run/deps.ts
+++ Run/deps.ts
@@ -1,3 +1,3 @@
 
-export * from "https://deno.land/std@0.201.0/path/mod.ts"; 
-export * from "https://raw.githubusercontent.com/nhrones/Config/main/mod.ts"
+export * from "https://deno.land/std@0.212.0/path/mod.ts";
+export * from "https://raw.githubusercontent.com/nhrones/Utilities/main/Config/mod.ts"
