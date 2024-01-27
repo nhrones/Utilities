f89172d23dc95c15b2ea5c0f874c0045e59b4e0c
NDH <nick@ndhrones.com>
2024-01-27 15:34:07 -0500

    hot refact

--- Hot/injector.ts
+++ Hot/injector.ts
@@ -1,21 +1,23 @@
 
 /**
- * This function recieves the raw text from index.html.  
- * We then replace the body-end-tag </body> tag with our 
- * custom WebSocket client code.   
+ * This function retrieves raw text from index.html.  
+ * It then replace the body-end-tag </body> tag with our 
+ * custom WebSocket Hot-Refresh client code. 
+ * @argument {string} fullPath - the full path to index.html 
  */
 export const inject = async (fullPath: string) => {
 
    const indexString = await Deno.readTextFile(fullPath)
-   const injectString = await Deno.readTextFile('./dist/wsClient.js')
    const endOfBody = indexString.indexOf('</body>')
-
+   // mahe sure we got an end tag
    if (endOfBody > 5) {
+      // just replace this tag with our script tag content
       const newBody = indexString.replace('</body>', `
-   <script type=module id="injected">
-${injectString}
+   <script type=module id="hot">
+const hotSocket = ${hotSocket.toString()}
+hotSocket()
    </script>
-   </body>`);
+   </body>`); // we've replaced the body end tag 
       return newBody
    } else {
       console.log('No </body> found!')
@@ -23,40 +25,30 @@ ${injectString}
    }
 }
 
-// /** The script string to inject */
-// const injectString = `(function () {
-//    const socket = new WebSocket(location.origin.replace("http", "ws"))
-//    console.log("CONNECTING")
-//    socket.onopen = () => {
-//       console.log("CONNECTED")
-//    }
-//    socket.onerror = () => {
-//       switch (socket.readyState) {
-//          case EventSource.CLOSED:
-//             console.log("DISCONNECTED")
-//             break
-//       }
-//    }
-//    socket.onmessage = (e) => {
-//       try {
-//          const action  = e.data
-//          console.log("sse got action - ", action)
-//          if (action === "refreshcss") {
-//             console.log("refreshCSS()")
-//             const sheets = [].slice.call(document.getElementsByTagName("link"))
-//             const head = document.getElementsByTagName("head")[0]
-//             for (let i = 0; i < sheets.length; ++i) {
-//                const elem = sheets[i]
-//                const parent = elem.parentElement || head
-//                parent.removeChild(elem)
-//                parent.appendChild(elem)
-//             }
-//          } else if (action === "reload") {
-//             console.log("Reload requested!")
-//             window.location.reload()
-//          }
-//       } catch (err) {
-//          console.info("err - ", err)
-//       }
-//    }
-// })()`
+/** This is the Hot-Socket client code to be injected */
+const hotSocket = () => {
+   console.log("HOT CONNECTING")
+   const socket = new WebSocket(location.origin.replace("http", "ws"))
+   socket.onopen = () => console.log("HOT CONNECTED")
+   socket.onerror = () => console.log("HOT DISCONNECTED")
+   socket.onmessage = (e) => {
+      const action = e.data
+      console.log("sse got action - ", action)
+      if (action === "refreshcss") {
+         console.log("refreshCSS()")
+         const sheets = [].slice.call(document.getElementsByTagName("link"))
+         const head = document.getElementsByTagName("head")[0]
+         for (let i = 0; i < sheets.length; ++i) {
+            const elem = sheets[i]
+            //@ts-ignore js code
+            const parent = elem.parentElement || head
+            parent.removeChild(elem)
+            parent.appendChild(elem)
+         }
+      } else if (action === "reload") {
+         console.log("Reload requested!")
+         window.location.reload()
+      }
+   }
+}
+
